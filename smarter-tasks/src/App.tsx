082e7bbd0165d33a7a7ca24be49b52f7a9627d8d
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-05-24 12:03:04 +0530

    added router

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,10 +1,22 @@
 import TaskApp from "./TaskApp";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
